d860dfde1ddf1f252a1898dbc998355201bd522f
Enrique Palacios <enriquepalaciosc@gmail.com>
2019-05-02 16:23:24 -0300

    Primer ejercicio de Socket

---
+++ AppSocket1/Comunicaciones/ClienteSocket.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSocket1.Comunicaciones
+{
+    public class ClienteSocket
+    {
+        private Socket cliente;
+        //enviar datos al socket
+        private StreamWriter writer;
+        //recibir datos desde el socket
+        private StreamReader reader;
+
+        public ClienteSocket(Socket cliente)
+        {
+            this.cliente = cliente;
+            Stream stream = new NetworkStream(this.cliente);
+            this.writer = new StreamWriter(stream);
+            this.reader = new StreamReader(stream);
+        }
+
+        public string Leer()
+        {
+            try
+            {
+                return reader.ReadLine().Trim();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public void Escribir(string mensaje)
+        {
+            try
+            {
+                this.writer.WriteLine(mensaje);
+                this.writer.Flush();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public void DesconectarCliente()
+        {
+            try
+            {
+                this.cliente.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
--- AppSocket1/Program.cs
+++ AppSocket1/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using AppSocket1.Comunicaciones;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +13,44 @@ namespace AppSocket1
     {
         static void Main(string[] args)
         {
-            string puertoTxt = ConfigurationManager.AppSettings["puerto"];
-            int puerto = Convert.ToInt32(puertoTxt);
-
+            // Obtenemos y convertimos a número el puerto en App.config
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
             string ip = ConfigurationManager.AppSettings["ip"];
+
+            // Levantamos un servidor en puerto indicado en App.config
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                Console.WriteLine($"Escuchando clientes en el puerto {puerto}");
+                Socket socketCliente = servidor.ObtenerCliente();
+                Console.WriteLine("Nuevo cliente conectado");
+                ClienteSocket clienteSocket = new ClienteSocket(socketCliente);
+
+
+                // Todo OK al iniciar
+                while (true)
+                {                   
+                    clienteSocket.Escribir("Hola mundo");
+                    string mensaje = clienteSocket.Leer();
+
+
+                    if (mensaje.ToLower().Equals("q"))  //mensaje == "q")
+                    {
+                        clienteSocket.DesconectarCliente();
+                        System.Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Respuesta: {mensaje}");
+                    }
+                }
+            }
+            else
+            {
+                // Falló al iniciar
+                Console.WriteLine("Falló al iniciar el servidor socket");
+            }
         }
     }
 }
